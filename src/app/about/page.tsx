9ffe83ff607f8fefed7c6df5a6f19ceda68f6e78
nettotkm <netto.mtx@gmail.com>
2023-10-10 20:19:58 -0300

    remove files

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,19 @@
 "use client";
+
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Hello</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div>Hello</div>
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Link href="/">Voltar</Link>
+      </div>
+    </main>
+  );
 }
